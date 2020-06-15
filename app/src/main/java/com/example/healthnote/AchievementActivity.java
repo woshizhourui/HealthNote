6d3f3fa2db9c59186b91d7b4636eee25378b809f
woshizhourui <zhouri_121@163.com>
2020-06-16 00:30:33 +0800

    成就页

--- app/src/main/java/com/example/healthnote/AchievementActivity.java
+++ app/src/main/java/com/example/healthnote/AchievementActivity.java
@@ -1,14 +1,68 @@
 package com.example.healthnote;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class AchievementActivity extends AppCompatActivity {
+    private ActivityAchievementBinding binding;
+    private List<TxtAndChooseBean> dataList;
+    private AchievementAdapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_achievement);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        binding = ActivityAchievementBinding.inflate(inflater);
+        setContentView(binding.getRoot());
+
+        initData();
+        initClick();
     }
+
+    private void initData() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 7);
+        binding.rv.setLayoutManager(gridLayoutManager);
+
+        createList();
+        List<RecordsBean> recordsBeans = DBflowUtil.getInstance().queryLisr(MyTimeUtil.getYear(), MyTimeUtil.getMonth());
+        LogUtil.e("----------->"+recordsBeans.size());
+        if (recordsBeans.size() > 0) {
+            for (int i = 0; i < dataList.size(); i++) {
+                for (int j = 0; j < recordsBeans.size(); j++) {
+                    if (dataList.get(i).getTitle().equals(recordsBeans.get(j).getDay())) {
+                        dataList.get(i).setStatus(recordsBeans.get(j).isFinish() ? 1 : 0);
+                    }
+                }
+            }
+        }
+
+        adapter = new AchievementAdapter(this, dataList, R.layout.rv_achievement);
+        binding.rv.setAdapter(adapter);
+    }
+
+    private void createList() {
+        dataList = new ArrayList<>();
+        int lastDay = MyTimeUtil.getCurrentMonthLastDay();
+        int day = Integer.valueOf(MyTimeUtil.getDay());
+        for (int i = 1; i <= lastDay; i++) {
+            dataList.add(new TxtAndChooseBean(i + "", i <= day ? 0 : 2));
+        }
+    }
+
+    private void initClick() {
+        binding.back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
 }
