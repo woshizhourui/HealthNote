b0731a770d15002a969f50b331e50fe485d978e6
woshizhourui <zhouri_121@163.com>
2020-06-16 23:55:53 +0800

    成就adapter

---
+++ app/src/main/java/com/example/healthnote/adapter/achievement.java
@@ -0,0 +1,25 @@
+package com.example.healthnote.adapter;
+
+import android.content.Context;
+import android.view.View;
+
+import com.example.healthnote.R;
+
+public class achievement extends MyRecyclerAdapter<TxtAndChooseBean>{
+    public AchievementAdapter(Context context, List<TxtAndChooseBean> mList, int mLayoutId) {
+        super(context, mList, mLayoutId);
+    }
+
+    @Override
+    public void convert(RecyclerViewHolder holder, TxtAndChooseBean data, int position) {
+        holder.setText(R.id.rv_achievement_txt, data.getTitle());
+        if (data.getStatus() == 2) {
+            holder.getView(R.id.rv_achievement_img).setVisibility(View.GONE);
+        } else {
+            holder.getView(R.id.rv_achievement_img).setVisibility(View.VISIBLE);
+            holder.setImageResource(R.id.rv_achievement_img, data.getStatus() == 0 ? R.drawable.cuohao : R.drawable.duihao);
+        }
+    }
+}
+
+}
